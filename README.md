24c595c0a646ceda154686afa6bc40c384fb9b6c
Benjamin Hsieh <bhsiehh@gmail.com>
2012-06-18 21:58:22 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GSLTRACKER
+
+Goals: create a simple, intuitive, easy to use resource for keeping up with GSL (and GSTL) results by combining data from GomTV.net and Liquipedia.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gsltracker::Application.config.secret_token = 'b3df29172b49fcd8a38a96860b49e6ba5e3535bfd4127ece8fd528137b8ff4dcbe943a2afa7a1b04cf74fc607a4f4196a418ff7136805f7591a1ab9325592155'
