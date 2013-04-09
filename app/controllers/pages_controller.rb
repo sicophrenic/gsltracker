dce20fae127645df5a2d908a66a4eba086eb0698
Benjamin Hsieh <bhsiehh@gmail.com>
2013-04-09 01:48:16 -0700

    reached a stable state (i hope...)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -18,29 +18,31 @@ class PagesController < ApplicationController
       page = agent.get('http://gomtv.net')
       content = page.content.gsub("\r","").gsub("\t","").gsub("\n","")
       @count = 0
-      content.match(/<dt id="promotion_#{@count}"(.*)<dt id="promotion_#{@count+1}"/)
+      content.match(/<!-- S:slide content -->(.*)<!-- E:slide content -->/)
+      content = $1
+      content.match(/(title=\".{30,80}\" ><img src=\"http:\/\/img.gomtv.net\/imgdata\/\d{5,10}\.jpg\")/)
       section = $1
       while section != nil do
-      # while @count < 3 do
-        section.match(/src="(.*jpg)"/)
+        content = content.gsub(section, "")
+        section.match(/src=\"(.*)\"/)
         img = $1
-        section.match(/title="(.*)"\s?class.*"tit"/)
+        section.match(/title=\"(.*)\" >/)
         info = $1
         data[img] = info
       	@count += 1
-        content.match(/<dt id="promotion_#{@count}"(.*)<dt id="promotion_#{@count+1}"/)
+        content.match(/(title=\".{30,80}\" ><img src=\"http:\/\/img.gomtv.net\/imgdata\/\d{5,10}\.jpg\")/)
         section = $1
       end
-      content.match(/<dt id="promotion_#{@count}"(.*)main_pro_num/)
-      section = $1
-      if section != nil
-        section.match(/src="(.*jpg)"/)
-        img = $1
-        section.match(/title="(.*)"\s?class.*"tit"/)
-        info = $1
-        data[img] = info
-        @count += 1
-      end
+      # content.match(/<dt id="promotion_#{@count}"(.*)main_pro_num/)
+      # section = $1
+      # if section != nil
+      #   section.match(/src="(.*jpg)"/)
+      #   img = $1
+      #   section.match(/title="(.*)"\s?class.*"tit"/)
+      #   info = $1
+      #   data[img] = info
+      #   @count += 1
+      # end
       # @count -= 1
       data
     end
